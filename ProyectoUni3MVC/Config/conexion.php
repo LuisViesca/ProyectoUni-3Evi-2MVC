07cf3bd39ec691e663bd11c7846c7175ad80bbcd
LuisViesca <73511335+LuisViesca@users.noreply.github.com>
2020-10-26 21:28:24 -0600

    Add files via upload

---
+++ ProyectoUni3MVC/Config/conexion.php
@@ -0,0 +1,9 @@
+<?php
+class conexion{
+  public static function conectar(){
+  $x = new PDO("mysql: host=localhost; dbname=zapatos; charset=utf8","root","");
+  $x->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $x;
+  }
+}
+?>
